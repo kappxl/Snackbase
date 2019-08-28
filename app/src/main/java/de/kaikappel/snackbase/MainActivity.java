5ba8e59b0d2c2430d04f6e74bc5332b405e68cf4
Kai Kappel <kaikappel@online.de>
2019-08-28 19:41:00 +0200

    Implement Filter and handle onbackpressed

--- app/src/main/java/de/kaikappel/snackbase/MainActivity.java
+++ app/src/main/java/de/kaikappel/snackbase/MainActivity.java
@@ -9,12 +9,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -43,4 +45,18 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (!(fragment instanceof IOnBackPressed) || !((IOnBackPressed) fragment).onBackPressed()) {
+            super.onBackPressed();
+        }
+    }
+
+    public void goHome() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+        bottomNav.setSelectedItemId(R.id.nav_home);
+    }
 }

353c3b3d4e95c79d11b3dcb7df35fa48be527904
Kai Kappel <kaikappel@online.de>
2019-08-29 17:17:46 +0200

    Prettier

--- app/src/main/java/de/kaikappel/snackbase/MainActivity.java
+++ app/src/main/java/de/kaikappel/snackbase/MainActivity.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
-    ArrayList<Food> mealList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
